213090f60530a8f821abbafb9ebd99af935a7df4
Phat Tran <phattm@msx.vn>
2020-08-29 03:45:36 +0700

    add prometheus

--- src/back-end/lib/Alidu.Core.ServiceHost/ConfigurationExtension.cs
+++ src/back-end/lib/Alidu.Core.ServiceHost/ConfigurationExtension.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Prometheus;
 using System;
 using System.Linq;
 
@@ -38,6 +39,7 @@ namespace Alidu.Core.ServiceHost
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapMetrics();
             });
         }
 
